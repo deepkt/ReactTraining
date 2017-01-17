2a0b789d2562dd526604f1136dc2f0885aa4b6a7
Pradeep Thoma <pradeep.thoma@compassitesinc.com>
2017-01-17 16:53:42 +0530

    Day2 commits

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,36 +1,80 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// var Greeting = require('./Greeting');
 import Greeting from './Greeting'
+
+var data = require('../helpers/data');
+
 var Home = React.createClass({
 
   getInitialState: function () {
     return{
-      user: ''
+      user: '',
+      dataLoaded: false,
+      userData:[],
+      query:''
     }
   },
 
-  handleNameChange: function(event){
-    // console.log(userData.getUsers());
-    this.setState({user: event.target.value});
+  componentDidMount: function () {
+    // Called when component is rendered completely
+    setTimeout(this.loadData, 3000);
   },
 
-  sayHello: function () {
+  loadData: function (){
+    this.setState({dataLoaded:true});
+    this.setState({userData:data.getUsers()});
+  },
+
+  handleQueryChange: function(event){
+    this.setState({query: event.target.value});
+  },
+
+  sayHello: function (sayName) {
     return(
       <div className='app-title'>
-        This is Inline(sayHello1) component - Ola {this.state.user}
+        This is Inline(sayHello1) component - Ola {sayName}
       </div>
     )
   },
 
+  loadingMessage: function(){
+    return(
+      <div>
+        Loading data....
+      </div>
+    );
+  },
+
+  mainContent: function(){
+    var users = this.state.userData;
+    var listOfUsers = [];
+    for(var i=0; i<users.length; i++){
+      if(users[i].name.toLowerCase().indexOf(this.state.query.toLowerCase()) > -1 ||
+                                this.state.query === ''){
+        listOfUsers.push(
+          <li>
+            <div>{users[i].name}</div>
+            <div>{users[i].email}</div>
+          </li>
+        );
+      }
+    }
+    return(
+      <div>
+        {listOfUsers}
+      </div>
+    );
+  },
+
   render() {
+    var currentContent = this.state.dataLoaded ? this.mainContent():this.loadingMessage();
     return(
       <div className='home-container'>
         <h1 className='app-title'>React JS with Grunt - Training</h1>
-        <input type='text' value={this.state.user} onChange={this.handleNameChange}/>
-        <Greeting username={this.state.user}/>
-        {this.sayHello()}
+        <input type='text' value={this.state.query} onChange={this.handleQueryChange}/>
+          {this.state.query}
+          {currentContent}
       </div>
     )
   }
